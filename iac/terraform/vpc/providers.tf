be2eb01e903e4de3551893e19f5d9f7ad6b49149
nivatony <84384521+nivatony@users.noreply.github.com>
2023-09-21 21:26:52 +0200

    Update providers.tf
    removed region = "global"

--- iac/terraform/vpc/providers.tf
+++ iac/terraform/vpc/providers.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "terraform-backend-salpad-1"
     key    = "terraform.tfstate/dev/vpc"
-    region = "global"
+    
   }
   required_providers {
     aws = {
