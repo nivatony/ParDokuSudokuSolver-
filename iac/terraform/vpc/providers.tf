619ff7f2547433cc26e1f3ece0c3e942ef95fd48
nivatony <84384521+nivatony@users.noreply.github.com>
2023-09-21 21:34:53 +0200

    Update providers.tf

--- iac/terraform/vpc/providers.tf
+++ iac/terraform/vpc/providers.tf
@@ -1,10 +1,4 @@
 terraform {
-  backend "s3" {
-    bucket = "terraform-backend-salpad-1"
-    key    = "terraform.tfstate/dev/vpc"
-    region = "us-east-1"
-    
-  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -14,5 +8,12 @@ terraform {
 }
 
 provider "aws" {
+  region = "us-east-1" # You can specify the AWS region here if it's different from the default region configured in your AWS CLI or environment.
+}
+
+backend "s3" {
+  bucket = "terraform-backend-salpad-1"
+  key    = "terraform.tfstate/dev/vpc"
+  region = "us-east-1"
 }
 
