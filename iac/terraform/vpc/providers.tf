9aab93908083109e137b3852f409cdf2f79bd435
nivatony <84384521+nivatony@users.noreply.github.com>
2023-09-22 19:04:14 +0200

    Update providers.tf

--- iac/terraform/vpc/providers.tf
+++ iac/terraform/vpc/providers.tf
@@ -8,12 +8,19 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1" # You can specify the AWS region here if it's different from the default region configured in your AWS CLI or environment.
+  region = "global" # You can specify the AWS region here if it's different from the default region configured in your AWS CLI or environment.
 }
 
-   backend "s3" {
-    bucket = "terraform-backend-salpad-1"
-    key    = "terraform.tfstate/dev/vpc"
-    region = "us-east-1"
+   provider "aws" {
+    region = "${var.aws_region}"
+}
+
+
+    backend "s3" {
+    bucket         = "terraform-backend-salpad-1"
+    key            = "terraform.tfstate/dev/vpc"
+    region         = "us-east-2"
+    encrypt        = true
+  }
 }
 
