25834250247c73d9f5ea2e5800d48796c074b763
nivatony <84384521+nivatony@users.noreply.github.com>
2023-09-22 21:31:12 +0200

    Update s3.bucket.tf
    terraform {
      backend "s3" {
        bucket         = "terraform-backend-salpad-1"
        key            = "terraform.tfstate/dev/vpc"
        region         = "us-east-2"
        encrypt        = true
      }
    }

--- iac/terraform/vpc/s3.bucket.tf
+++ iac/terraform/vpc/s3.bucket.tf
@@ -1,6 +1,8 @@
-
+terraform {
+  backend "s3" {
     bucket         = "terraform-backend-salpad-1"
     key            = "terraform.tfstate/dev/vpc"
     region         = "us-east-2"
     encrypt        = true
-
+  }
+}
