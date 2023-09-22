8d18f8f0bc19ddaa0efdfdc2d263d856ee877e2b
nivatony <84384521+nivatony@users.noreply.github.com>
2023-09-22 19:33:39 +0200

    Update s3.bucket.tf
    removed }{

--- iac/terraform/vpc/s3.bucket.tf
+++ iac/terraform/vpc/s3.bucket.tf
@@ -1,6 +1,6 @@
-  backend "s3" {
+
     bucket         = "terraform-backend-salpad-1"
     key            = "terraform.tfstate/dev/vpc"
     region         = "us-east-2"
     encrypt        = true
-  }
+
