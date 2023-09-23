327c9e5268ced0a64518df7cad426fb0bb9311bc
nivatony <84384521+nivatony@users.noreply.github.com>
2023-09-23 13:24:24 +0200

    Update s3.bucket.tf
    bucket         = "cf-templates-1gvzivy3mwpjd-eu-north-1" changed to this

--- iac/terraform/vpc/s3.bucket.tf
+++ iac/terraform/vpc/s3.bucket.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-backend-salpad"
+    bucket         = "cf-templates-1gvzivy3mwpjd-eu-north-1"
     key            = "terraform.tfstate/dev/vpc"
     region         = "eu-north-1"
     encrypt        = true
