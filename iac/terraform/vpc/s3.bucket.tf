2eb50a0871dcfd8b978d31adda150a64446aeb96
nivatony <84384521+nivatony@users.noreply.github.com>
2023-09-23 13:08:40 +0200

    Update s3.bucket.tf
    eu-north-1

--- iac/terraform/vpc/s3.bucket.tf
+++ iac/terraform/vpc/s3.bucket.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "terraform-backend-salpad"
     key            = "terraform.tfstate/dev/vpc"
-    region         = "global"
+    region         = "eu-north-1"
     encrypt        = true
   }
 }
