62db58bf22de852a301435c961290e4779efc110
nivatony <nivatony3@gmail.com>
2023-09-24 18:57:06 +0200

    sudoku-solver with ECR and EKS add to main.tf and added output.ft with r53 in vpc folder

---
+++ iac/terraform/Eks/ecr.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"  # Replace with your desired region
+}
+
+resource "aws_ecr_repository" "sudoku_solver" {
+  name = "sudoku-solver-app"
+}
+
+output "ecr_repository_url" {
+  value = aws_ecr_repository.sudoku_solver.repository_url
+}
+
