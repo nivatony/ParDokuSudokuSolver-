294ef79fb878848c4645e84696e53c1fb1c0178b
nivatony <84384521+nivatony@users.noreply.github.com>
2023-09-26 13:52:45 +0200

    Update output.tf

--- iac/terraform/vpc/output.tf
+++ iac/terraform/vpc/output.tf
@@ -1,16 +1,23 @@
-output "ecr_repository_url" {
-  value = aws_ecr_repository.sudoku_solver.repository_url
+output "ec2_instance_id" {
+  value = aws_instance.my_eks_instance.id
 }
 
-# Output the EKS cluster endpoint
+output "kubeconfig" {
+  value = data.aws_eks_cluster_auth.my_cluster.kubeconfig
+}
+
+output "ecr_repository_url" {
+  value = aws_ecr_repository.my_repository.repository_url
+}
+
 output "eks_cluster_endpoint" {
   value = module.eks_cluster.cluster_endpoint
 }
 
-# Output the EKS cluster certificate authority data
 output "eks_cluster_ca_data" {
   value = module.eks_cluster.cluster_certificate_authority_data
 }
+
 
 # Define other resources and configurations as needed..
 
