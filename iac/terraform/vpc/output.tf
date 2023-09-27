80ea140ae3a22b24ac8baa8892ad1bd120d2cd7a
nivatony <84384521+nivatony@users.noreply.github.com>
2023-09-27 22:19:30 +0200

    Update output.tf

--- iac/terraform/vpc/output.tf
+++ iac/terraform/vpc/output.tf
@@ -1,5 +1,15 @@
+output "kubeconfig-certificate-authority-data" {
+  value = aws_eks_cluster.my_cluster.certificate_authority[0].data
 
+}
 
+output "cluster_endpoint" {
+  value = aws_eks_cluster.my_cluster.endpoint
+}
+
+output "cluster_id" {
+  value = aws_eks_cluster.my_cluster.id
+}
 
 output "ecr_repository_url" {
   value = aws_ecr_repository.sudoku_solver_app1.repository_url
