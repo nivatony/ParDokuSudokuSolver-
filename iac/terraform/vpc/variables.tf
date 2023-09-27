a461102d611bd18edb761f0237eaed868d929829
nivatony <84384521+nivatony@users.noreply.github.com>
2023-09-27 15:55:12 +0200

    Update variables.tf
    variable "cluster_name" {
      type = string
    }
    
    variable "environment" {
      type = string
    }
    
    variable "vpc_cidr_block" {
      type = string
    }
    
    variable "public_subnet_1_details" {
      type = object({
        availability_zone = string
        cidr_block        = string
      })
    }
    
    variable "public_subnet_2_details" {
      type = object({
        availability_zone = string
        cidr_block        = string
      })
    }
    
    variable "private_subnet_1_details" {
      type = object({
        availability_zone = string
        cidr_block        = string
      })
    }
    
    variable "private_subnet_2_details" {
      type = object({
        availability_zone = string
        cidr_block        = string
      })
    }
    
    variable "eks_node_group_role_name" {
      description = "The name of the IAM role for the EKS node group."
    }
    
    variable "eks_worker_node_policy_arn" {
      description = "The ARN of the AmazonEKSWorkerNodePolicy."
    }
    
    variable "eks_cni_policy_arn" {
      description = "The ARN of the AmazonEKS_CNI_Policy."
    }
    
    variable "ec2container_policy_arn" {
      description = "The ARN of the AmazonEC2ContainerRegistryReadOnly."
    }
    
    variable "eks_vpc_resource_controller_policy_arn" {
      description = "The ARN of the AmazonEKSVPCResourceController."
    }

--- iac/terraform/vpc/variables.tf
+++ iac/terraform/vpc/variables.tf
@@ -37,3 +37,25 @@ variable "private_subnet_2_details" {
     cidr_block        = string
   })
 }
+
+variable "eks_node_group_role_name" {
+  description = "The name of the IAM role for the EKS node group."
+}
+
+variable "eks_worker_node_policy_arn" {
+  description = "The ARN of the AmazonEKSWorkerNodePolicy."
+}
+
+variable "eks_cni_policy_arn" {
+  description = "The ARN of the AmazonEKS_CNI_Policy."
+}
+
+variable "ec2container_policy_arn" {
+  description = "The ARN of the AmazonEC2ContainerRegistryReadOnly."
+}
+
+variable "eks_vpc_resource_controller_policy_arn" {
+  description = "The ARN of the AmazonEKSVPCResourceController."
+}
+
+
