716abedb7a05172cf23645d589cf50984cac8de5
nivatony <84384521+nivatony@users.noreply.github.com>
2023-09-27 22:41:45 +0200

    Update output.tf

--- iac/terraform/vpc/output.tf
+++ iac/terraform/vpc/output.tf
@@ -19,6 +19,11 @@ output "aws_public_subnet" {
   value = aws_subnet.public_subnet.*.id
 }
 
+output "aws_private_subnet" {
+  value = aws_subnet.private_subnet.*.id
+}
+
+
 output "vpc_id" {
   value = aws_vpc.main.id
 }
