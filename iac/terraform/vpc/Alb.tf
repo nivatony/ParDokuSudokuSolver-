6483667996895dc4afa8609ce888bd370e019a42
nivatony <84384521+nivatony@users.noreply.github.com>
2023-09-28 14:48:21 +0200

    Update Alb.tf

--- iac/terraform/vpc/Alb.tf
+++ iac/terraform/vpc/Alb.tf
@@ -1,5 +1,5 @@
 resource "aws_lb" "my_alb" {
-  name               = "${var.cluster_name}_alb1"
+  name               = "my-alb"
   internal           = false
   load_balancer_type = "application"
   subnets            = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
