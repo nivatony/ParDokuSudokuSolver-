fec45d5d62f4064fb00260762bf52fcabb99d6de
nivatony <84384521+nivatony@users.noreply.github.com>
2023-09-28 12:17:24 +0200

    Delete iac/terraform/vpc/Igw.tf

--- iac/terraform/vpc/Igw.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_internet_gateway" "internet_gw" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    "Name"        = "${var.cluster_name}-${var.environment}-internet-gateway"
-    "ClusterName" = var.cluster_name
-    "Environment" = var.environment
-  }
-}
