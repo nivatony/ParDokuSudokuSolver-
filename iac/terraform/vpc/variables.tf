34369b9a0827a439624ffa94ecd6a58ca6c1c286
nivatony <84384521+nivatony@users.noreply.github.com>
2023-09-28 12:47:56 +0200

    Update variables.tf

--- iac/terraform/vpc/variables.tf
+++ iac/terraform/vpc/variables.tf
@@ -39,12 +39,18 @@ variable "private_subnet_2_details" {
 }
 
 
-variable "aws_lb.my_alb" {
-  type = object({
-    availability_zone = string
-    cidr_block        = string
-    })
+variable "my_alb" {
+  description = "Reference to the ALB resource"
+  type        = object({
+    arn          = string
+    dns_name     = string
+    id           = string
+    load_balancer_type = string
+    zone_id      = string
+  })
+  default     = null
 }
+
 
 
 variable "node_group_name" {}
