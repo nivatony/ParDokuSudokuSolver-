74d9b0095d19e4565984043b0655ce4cfb60f399
nivatony <84384521+nivatony@users.noreply.github.com>
2023-09-28 16:36:29 +0200

    Update main.tf

--- iac/terraform/vpc/main.tf
+++ iac/terraform/vpc/main.tf
@@ -125,6 +125,13 @@ resource "aws_eks_cluster" "my_cluster" {
   }
 }
 
+resource "aws_lb_target_group" "my_tg" {
+  name     = "my-tg"
+  target_type = "instance"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+}
 
 
 resource "aws_security_group" "node_group_one" {
