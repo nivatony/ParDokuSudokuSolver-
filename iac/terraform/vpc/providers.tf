0778a2452b2f0ee93c87f7ca0b3f19e46f06016a
nivatony <84384521+nivatony@users.noreply.github.com>
2023-09-29 23:20:42 +0200

    Update providers.tf
    provider "kubernetes" {
      alias   = "eks"
      host    = aws_eks_cluster.my_cluster.endpoint
      cluster_ca_certificate = base64decode(aws_eks_cluster.my_cluster.certificate_authority.0.data)
      token   = data.aws_eks_cluster_auth.my_cluster.token
      load_config_file       = false
    }

--- iac/terraform/vpc/providers.tf
+++ iac/terraform/vpc/providers.tf
@@ -7,10 +7,3 @@ terraform {
   }
 }
 
-provider "kubernetes" {
-  alias   = "eks"
-  host    = aws_eks_cluster.my_cluster.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.my_cluster.certificate_authority.0.data)
-  token   = data.aws_eks_cluster_auth.my_cluster.token
-  load_config_file       = false
-}
