ba2a853f217ad3e9d73389fa9c259fa4f953ad27
nivatony <84384521+nivatony@users.noreply.github.com>
2023-09-30 12:19:57 +0200

    Update providers.tf

--- iac/terraform/vpc/providers.tf
+++ iac/terraform/vpc/providers.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "kubernetes" {
-  config_path    = pathexpand("./ParDokuSudokuSolver-/.kube_config.yaml")  # Set to the correct path of your kubeconfig file
+  config_path    = pathexpand("./ParDokuSudokuSolver-/config")  # Set to the correct path of your kubeconfig file
   config_context = "arn:aws:eks:eu-north-1:712699700534:cluster/awesome_cluster"
 }
 
