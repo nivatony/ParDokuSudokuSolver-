0d6c28068d9215de466a9c5b4e9c7497a7f2c406
nivatony <84384521+nivatony@users.noreply.github.com>
2023-09-30 15:14:59 +0200

    Update providers.tf

--- iac/terraform/vpc/providers.tf
+++ iac/terraform/vpc/providers.tf
@@ -7,6 +7,18 @@ terraform {
   } 
 }
 
+
+provider "kubernetes" {
+  host = data.aws_eks_cluster.my_cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args = ["eks", "get-token", "--cluster-name", var.cluster_name]
+    command = "aws"
+ }
+}
+
+
 #provider "kubernetes" {
             
  # config_path    = "ParDokuSudokuSolver-/.kube/config" # Set to the correct path of your kubeconfig file
