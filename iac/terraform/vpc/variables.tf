21d88e0f6fab0e04f5d912f0e525c49af294fd4b
nivatony <84384521+nivatony@users.noreply.github.com>
2023-09-30 19:26:42 +0200

    Update variables.tf

--- iac/terraform/vpc/variables.tf
+++ iac/terraform/vpc/variables.tf
@@ -2,23 +2,19 @@ variable "cluster_name" {
   type = string
 }
 
-variable "environment" {
-  type = string
-}
-
 variable "desired_size" {
   type    = number
-  
+  default = 1
 }
 
 variable "max_size" {
   type    = number
-
+  default = 1
 }
 
 variable "min_size" {
   type    = number
-
+  default = 1
 }
 
 
