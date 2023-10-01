017ca3c0bc64008d027a2b642e9ef76c9bb099cb
nivatony <84384521+nivatony@users.noreply.github.com>
2023-10-01 16:46:16 +0200

    Update eks-node-groups.tf

--- iac/terraform/Eks/eks-node-groups.tf
+++ iac/terraform/Eks/eks-node-groups.tf
@@ -22,7 +22,6 @@ resource "aws_iam_role" "my-node-group" {
 }
 POLICY
 }
-
    
 # Resource: aws_iam_role_policy_attachment
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role_policy_attachment
