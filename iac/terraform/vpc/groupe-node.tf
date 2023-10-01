e7480ccb2f82da06f9ba7741e92b628252157b33
nivatony <84384521+nivatony@users.noreply.github.com>
2023-10-01 21:04:01 +0200

    Update groupe-node.tf

--- iac/terraform/vpc/groupe-node.tf
+++ iac/terraform/vpc/groupe-node.tf
@@ -2,7 +2,7 @@
 resource "aws_eks_node_group" "my-node-group" {
   cluster_name = aws_eks_cluster.my_cluster.name
   node_group_name = "my-node-group"
-  node_role_arn = arn:aws:iam::712699700534:role/github-actions-role
+  node_role_arn = "arn:aws:iam::712699700534:role/github-actions-role"
   subnet_ids = [
     aws_subnet.public_subnet_1.id,
     aws_subnet.public_subnet_2.id
@@ -14,9 +14,9 @@ resource "aws_eks_node_group" "my-node-group" {
     min_size     = 1
   }
 
-  ami_type = "AL2_x86_64"
-  capacity_type = "ON_DEMAND"
-  disk_size = 20
+  ami_type       = "AL2_x86_64"
+  capacity_type  = "ON_DEMAND"
+  disk_size      = 20
   instance_types = ["t3.small"]
-  key_name = "ubuntu
+  key_name       = "ubuntu"
 }
