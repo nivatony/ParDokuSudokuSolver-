be844f90f2ea489316633733d07d025b4105f372
nivatony <84384521+nivatony@users.noreply.github.com>
2023-10-02 17:10:25 +0200

    Update routetable.tf
    resource "aws_route_table" "public_rt" {
      vpc_id = aws_vpc.main.id
    
      route {
        cidr_block = "0.0.0.0/0"
        gateway_id = aws_internet_gateway.internet_gw.id
      }
    
      tags = {
        Name = "public route"
      }
    }

--- iac/terraform/vpc/routetable.tf
+++ iac/terraform/vpc/routetable.tf
@@ -11,6 +11,20 @@ resource "aws_route_table" "public_rt" {
   }
 }
 
+
+
+resource "aws_route_table" "public_rt2" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gw.id
+  }
+
+  tags = {
+    Name = "public route"
+  }
+}
 resource "aws_route_table" "private1" {
   # The VPC ID.
   vpc_id = aws_vpc.main.id
