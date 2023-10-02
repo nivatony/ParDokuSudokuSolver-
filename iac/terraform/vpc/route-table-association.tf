fc46213dfdc925a986c12919147d67a8e74dc0bb
nivatony <84384521+nivatony@users.noreply.github.com>
2023-10-02 17:09:26 +0200

    Update route-table-association.tf

--- iac/terraform/vpc/route-table-association.tf
+++ iac/terraform/vpc/route-table-association.tf
@@ -3,7 +3,7 @@
 # Assuming you have declared the above resources, you can now create route table associations
 resource "aws_route_table_association" "public1" {
   subnet_id      = aws_subnet.public_subnet_1.id
-  route_table_id = aws_route_table.public.id
+  route_table_id = aws_route_table.public_rt.id
 }
 
 resource "aws_route_table_association" "public2" {
