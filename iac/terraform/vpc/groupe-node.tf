8f2181b19c01f6cfc4835a7324e8db9aba47948e
nivatony <84384521+nivatony@users.noreply.github.com>
2023-10-02 10:48:20 +0200

    Update groupe-node.tf

--- iac/terraform/vpc/groupe-node.tf
+++ iac/terraform/vpc/groupe-node.tf
@@ -16,7 +16,7 @@ resource "aws_eks_node_group" "my-node-group" {
 
   ami_type       = "AL2_x86_64"
   capacity_type  = "ON_DEMAND"
-  disk_size      = 20
+  disk_size      = 20 
   instance_types = ["t3.small"]
    
 }     
