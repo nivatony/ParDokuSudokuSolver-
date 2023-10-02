8a316001aaf9c50f5748c888e6e3eee2309c0120
nivatony <84384521+nivatony@users.noreply.github.com>
2023-10-02 16:46:54 +0200

    Update routetable.tf

--- iac/terraform/vpc/routetable.tf
+++ iac/terraform/vpc/routetable.tf
@@ -10,3 +10,39 @@ resource "aws_route_table" "public_rt" {
     Name = "public route"
   }
 }
+
+resource "aws_route_table" "private1" {
+  # The VPC ID.
+  vpc_id = aws_vpc.main.id
+
+  route {
+    # The CIDR block of the route.
+    cidr_block = "0.0.0.0/0"
+
+    # Identifier of a VPC NAT gateway.
+    nat_gateway_id = aws_nat_gateway.gw1.id
+  }
+
+  # A map of tags to assign to the resource.
+  tags = {
+    Name = "private1"
+  }
+}
+
+resource "aws_route_table" "private2" {
+  # The VPC ID.
+  vpc_id = aws_vpc.main.id
+
+  route {
+    # The CIDR block of the route.
+    cidr_block = "0.0.0.0/0"
+
+    # Identifier of a VPC NAT gateway.
+    nat_gateway_id = aws_nat_gateway.gw2.id
+  }
+
+  # A map of tags to assign to the resource.
+  tags = {
+    Name = "private2"
+  }
+}
