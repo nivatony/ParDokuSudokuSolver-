73b0a7e49526daaf913b6fdba9b10fbe2f1430d8
nivatony <84384521+nivatony@users.noreply.github.com>
2023-10-02 14:44:18 +0200

    Update main.tf

--- iac/terraform/vpc/main.tf
+++ iac/terraform/vpc/main.tf
@@ -172,27 +172,6 @@ POLICY
 
 
 
-# Resource: aws_iam_role_policy_attachment
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role_policy_attachment
-
-resource "aws_iam_role_policy_attachment" "amazon_eks_worker_node_policy_general" {
-  # The ARN of the policy you want to apply.
-  # https://github.com/SummitRoute/aws_managed_policies/blob/master/policies/AmazonEKSWorkerNodePolicy
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-
-  # The role the policy should be applied to
-  role = aws_iam_role.nodes_general.name
-}
-
-resource "aws_iam_role_policy_attachment" "amazon_eks_cni_policy_general" {
-  # The ARN of the policy you want to apply.
-  # https://github.com/SummitRoute/aws_managed_policies/blob/master/policies/AmazonEKS_CNI_Policy
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-
-  # The role the policy should be applied to
-  role = aws_iam_role.nodes_general.name
-}
-
 
 
 
