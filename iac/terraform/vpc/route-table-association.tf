72f498987925dbb19e880252ed3b0722fe4918ce
nivatony <84384521+nivatony@users.noreply.github.com>
2023-10-02 17:03:48 +0200

    Update route-table-association.tf

--- iac/terraform/vpc/route-table-association.tf
+++ iac/terraform/vpc/route-table-association.tf
@@ -1,19 +1,4 @@
-# Assuming you have subnet resources defined
-resource "aws_subnet" "public_subnet_1" {
-  # Configuration for public subnet 1
-}
 
-resource "aws_subnet" "public_subnet_2" {
-  # Configuration for public subnet 2
-}
-
-resource "aws_subnet" "private_subnet_1" {
-  # Configuration for private subnet 1
-}
-
-resource "aws_subnet" "private_subnet_2" {
-  # Configuration for private subnet 2
-}
 
 # Assuming you have declared the above resources, you can now create route table associations
 resource "aws_route_table_association" "public1" {
